0da9585b989cb11778e1b7c8d15b7ebb8dc670e0
Prakash-1012 <mishuraj123456789@gmail.com>
2025-09-12 01:22:11 +0530

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,47 @@
+import { useRef } from "react";
 import { useEffect, useState } from "react";
 
 function App() {
-const[cardStyle,setCardStyle]=useState({
-  width:"200px",
-  border:"2px solid #343a40",
-  boxShadow:"1px 1px 10px 1px #0287fbff",
-  margin:"20px"
-})
-const updateTheme=(bgcolor,textColor)=>{
-setCardStyle({...cardStyle,backgroundColor:bgcolor,color:textColor});
-}
-    return (
+
+   const divRef=useRef(null);
+   const inputRef=useRef(null);
+   const handleInputRef=()=>{
+      inputRef.current.focus();
+      inputRef.current.value="Prakash ranjan"
+   }
+   const handleDiv=()=>{
+      if( divRef.current.style.display!="none"){
+          divRef.current.style.display="none"
+      }
+      else{
+          divRef.current.style.display="inline"
+      }
+
+  
+   }
+
+    return(
+      
       <div>
-       <button onClick={()=>updateTheme("gray","green")}>New Theme</button>
-          <button onClick={()=>updateTheme("white","black")}>Deafult theme</button>
-        <div style={{display:"flex",flexWrap:"wrap"}}>
-        
-         <div style={cardStyle}>
+         <h1>UseRef</h1>
+         <div>
+             <div >
              <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
+             <div  style={{padding:"10px"}}>
               <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
+              <p ref={divRef}>Frontend Developer</p>
              </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-          <div style={cardStyle}>
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p>Frontend Developer</p>
-             </div>
-             
-          </div>
-        </div>
-        </div>
+         </div>
+      </div>
+      <button onClick={handleDiv}>Change text color</button>
+      <br />
+      <br />
+      <button onClick={handleInputRef}>focus on input field</button>
+      <br />
+      <br />      
+      <input ref={inputRef} type="text" placeholder="enter your name" />
+      </div>
+
     )
 
 }

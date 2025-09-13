c5a0dd2df245bf5fb718296c8887d1410c8a77fd
Prakash-1012 <mishuraj123456789@gmail.com>
2025-09-14 02:57:44 +0530

    uncontrolled

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,35 @@ import { useRef } from "react";
 import { useEffect, useState } from "react";
 
 function App() {
-
-   const divRef=useRef(null);
-   const inputRef=useRef(null);
-   const handleInputRef=()=>{
-      inputRef.current.focus();
-      inputRef.current.value="Prakash ranjan"
-   }
-   const handleDiv=()=>{
-      if( divRef.current.style.display!="none"){
-          divRef.current.style.display="none"
-      }
-      else{
-          divRef.current.style.display="inline"
-      }
-
   
+
+   const handleInput=(evt)=>{
+   const user=document.querySelector("#user").value;
+   const password=document.querySelector("#password").value;
+      evt.preventDefault();
+      console.log(user);
+      console.log(password);
    }
+
+
+
+   
 
     return(
       
-      <div>
-         <h1>UseRef</h1>
-         <div>
-             <div >
-             <img style={{width:"200px",boxShadow:"1px 1px 10px 1px #343a40",}}src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-             <div  style={{padding:"10px"}}>
-              <h3>Prakash Ranjan</h3>
-              <p ref={divRef}>Frontend Developer</p>
-             </div>
-         </div>
-      </div>
-      <button onClick={handleDiv}>Change text color</button>
+      <div> 
+      <form action="" onSubmit={handleInput}>
+      <input id="user" type="text" placeholder="Username" />
       <br />
       <br />
-      <button onClick={handleInputRef}>focus on input field</button>
+      <input id="password" type="password" placeholder="Password" />
       <br />
-      <br />      
-      <input ref={inputRef} type="text" placeholder="enter your name" />
+      <br />
+      <button>
+         Submit
+      </button>
+      </form>
+
       </div>
 
     )

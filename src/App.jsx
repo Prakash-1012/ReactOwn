05c7f192c17aed0d09809f682d8caaf662c2724f
Prakash-1012 <mishuraj123456789@gmail.com>
2025-09-14 05:48:02 +0530

    UseFormStatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 import { useRef } from "react";
 import { useEffect, useState } from "react";
-import ForwardRef from "./ForwardRef";
+import { useFormStatus } from "react-dom";
+
 function App() {
-const inputRef=useRef(null);
-function handleRef(){
-   inputRef.current.value=1000;
-   inputRef.current.focus();
-   inputRef.current.style.color="blue";
-}
+
+   const handleForm=async()=>{
+      await new Promise(res=>setTimeout(res,5000));
+      console.log("submit");
+   }
+
+   function Form(){
+      const {pending}=useFormStatus();
+      console.log(pending);
+      
+      return(
+         <div>
+         <input type="text" placeholder="enter username" />
+         <br />
+         <br />
+         <input type="password" placeholder="enter Password" />
+
+         <button disabled={pending}>{pending?"submitting...":"submit"}</button>
+         </div>
+      )
+   }
+
+
   
    return(
       <div>
-         <ForwardRef ref={inputRef}/>
-         <hr />
-         <button onClick={handleRef}>
-            Forward Ref
-         </button>
+        <form action={handleForm} >
+        <Form/>
 
+        </form>
 
       </div>
    )
--- src/ForwardRef.jsx
+++
@@ -1,8 +0,0 @@
-const ForwardRef=(props)=>{
-    return(
-        <div>
-            <input ref={props.ref}type="text" placeholder="enter value" />
-        </div>
-    )
-}
-export default ForwardRef;

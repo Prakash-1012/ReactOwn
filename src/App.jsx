cbe11768be33e2dc1f2873a21a0fb08a4b022750
Prakash-1012 <mishuraj123456789@gmail.com>
2025-09-14 06:25:54 +0530

    useTransition

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import { useRef } from "react";
+import { useRef, useTransition } from "react";
 import { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
 
 function App() {
+const [pending,startTransition]=useTransition();
+const handlebutton=()=>{
+startTransition(async()=>{
+   await new Promise(res=>setTimeout(res,3000))
 
-   const handleForm=async()=>{
-      await new Promise(res=>setTimeout(res,5000));
-      console.log("submit");
-   }
+})
 
-   function Form(){
-      const {pending}=useFormStatus();
-      console.log(pending);
-      
-      return(
-         <div>
-         <input type="text" placeholder="enter username" />
-         <br />
-         <br />
-         <input type="password" placeholder="enter Password" />
-
-         <button disabled={pending}>{pending?"submitting...":"submit"}</button>
-         </div>
-      )
-   }
-
-
-  
+}
    return(
       <div>
-        <form action={handleForm} >
-        <Form/>
-
-        </form>
+         {
+            pending?<img style={{width:"200px"}} src="https://res.cloudinary.com/bytesizedpieces/image/upload/v1656084931/article/a-how-to-guide-on-making-an-animated-loading-image-for-a-website/animated_loader_gif_n6b5x0.gif" />:null 
+         }
+         <button disabled={pending} onClick={handlebutton}>click me!</button>
 
       </div>
    )

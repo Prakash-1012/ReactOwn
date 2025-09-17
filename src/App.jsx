5c7a576e4304d91a8ece86f2cf5f75ef4cb82dc2
Prakash-1012 <mishuraj123456789@gmail.com>
2025-09-18 01:30:57 +0530

    derived-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import { useRef, useTransition } from "react";
 import { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
-let count = 0;
 function App() {
 
-// impure component
-  const Counter = () => {
-    count = count + 2;
-    return (
-      <div>
-        <h1>counter:{count}</h1>
-      </div>
-    )
-  }
+const[users,setUsers]=useState([]);
+const[user,setUser]=useState();
+
+const handleUsers=()=>{
+  setUsers([...users,user]);
+}
+// derived states
+let total_user=users.length;
+let last_user=users[total_user-1];
+let unique_users=[...new Set(users)].length;
+
+
 
 
   return (
     <div>
-
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter/>
-
+      <input type="text" placeholder="add new user" onChange={(evt)=>setUser(evt.target.value)} />
+      <button onClick={handleUsers}>Add user</button>
+      <h1>Total users:{total_user}</h1>
+      <h1>Last user:{last_user}</h1>
+      <h1>Unique users:{unique_users}</h1>
 
     </div>
   )

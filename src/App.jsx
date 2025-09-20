71cccc086b1775010595155659ed99ab18be7d81
Prakash-1012 <mishuraj123456789@gmail.com>
2025-09-20 15:43:07 +0530

    useId

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
-import { useRef, useTransition } from "react";
+import { useId, useRef, useTransition } from "react";
 import { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
-import AddUser from "./AddUser";
-import DisplayUser from "./DisplayUser";
+
 function App() {
 
-// lifting state up
-const [user, setUser]=useState();
+
 
 
 
 
   return (
     <div>
-     <AddUser setUser={setUser}/>
-     <DisplayUser user={user}/>
+  <Users />
+  <Users />
+
+
 
     </div>
   )
 
 }
 export default App;
+function Users(){
+  const user=useId();
+  return(
+    <div>
+     <input type="text" id={user+"name"} placeholder="enter username"/>
+     <label htmlFor={user+"name"}>enter name</label>
+     <br />
+     <br />
+      <input type="password" id={user+"password"} placeholder="enter password"/>
+     <label htmlFor={user+"password"}>enter password</label>
+     <br />
+     <br />
+      <input type="text" id={user+"skills"} placeholder="enter skills"/>
+     <label htmlFor={user+"skills"}>enter skills</label>
+     <br />
+     <br />
+     <hr />
+
+    </div>
+  )
+}

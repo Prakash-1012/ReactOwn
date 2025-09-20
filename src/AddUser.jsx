5f572055f3b253bb38db5616d127e813a405e9e6
Prakash Ranjan <mishuraj123456789@gmail.com>
2025-09-20 15:23:50 +0530

    Delete src/AddUser.jsx

--- src/AddUser.jsx
+++
@@ -1,8 +0,0 @@
-const AddUser=({setUser})=>{
-    return(
-        <div>
-<input type="text" placeholder="Add new user" onChange={(evt)=>setUser(evt.target.value)} />
-        </div>
-    )
-}
-export default AddUser;

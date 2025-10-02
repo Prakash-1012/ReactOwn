c0e6dd8d649cfe53cad787681db8ba85ae967f5d
Prakash-1012 <mishuraj123456789@gmail.com>
2025-10-03 04:19:24 +0530

    Lazy-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import "./index.css";
 function App() {
-const[nameErr,setNameErr]=useState();
+const Users=lazy(()=>import("./Users"))
+const[load,setLoad]=useState(false);
 
-const[passErr,setPassErr]=useState();
 
-
-const handleName=(evt)=>{
-  if(evt.target.value.length>5){
-    setNameErr("Length of name exceeded(only 5 characters)");
-  }else{
-    setNameErr();
-  }
-
-}
-const handlePass=(evt)=>{
-  let regex=/^[A-Z0-9]+$/i;
-  if(regex.test(evt.target.value)){
-    setPassErr();
-  }
-  else{
-    setPassErr("No special characters allowed")
-  }
-
-}
 
 
   return (
     <>
-  <input className={nameErr?"error":""} type="text" onChange={handleName} placeholder="enter your name"  />
-  <span style={{color:"red"}}>{nameErr && nameErr}</span>
-  <br />
-  <br />
-  <input className={passErr?"error":""} type="text" onChange={handlePass} placeholder="enter your password"  />
-  <span style={{color:"red"}}>{passErr && passErr}</span>
-
-  <br />
-  <br />
-  <button disabled={nameErr||passErr}>Login</button>
+    <h1>Lazy Loading</h1>
+  <button onClick={()=>setLoad(true)}>Load Users</button>
+  {
+    load?<Suspense fallback={<p>loading...</p>}><Users/></Suspense>:null
+  }
 
     </>
   )
---
+++ src/Users.jsx
@@ -0,0 +1,8 @@
+export default function Users(){
+    return(
+        <>
+        <h3>Prakash</h3>
+        <h3>Priya</h3>
+        </>
+    )
+}

9cc98d9268b56982a7518597a7258e309ebe0712
Prakash-1012 <mishuraj123456789@gmail.com>
2025-10-03 06:08:14 +0530

    useActionState-validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import { lazy, Suspense, useEffect, useState } from "react";
+import { lazy, Suspense, useActionState, useEffect, useState } from "react";
 import "./index.css";
 function App() {
-const Users=lazy(()=>import("./Users"))
-const[load,setLoad]=useState(false);
+  const handleValidation = (prevData, formData) => {
+    let regex = /[A-Z0-9]+$/i;
+    let name = formData.get("name");
+    let password = formData.get("password");
+    if (name.length > 5) {
+      return { error: "Name should only contain 5 characters",name,password };
+    }
+    else if (!regex.test(password)) {
+      return { error: "No special characters allowed",name,password };
+    }
+    else {
+      return { message: "Login done",name,password };
+    }
 
+  }
+
+  const [data, action, pending] = useActionState(handleValidation, undefined);
 
 
 
   return (
     <>
-    <h1>Lazy Loading</h1>
-  <button onClick={()=>setLoad(true)}>Load Users</button>
-  {
-    load?<Suspense fallback={<p>loading...</p>}><Users/></Suspense>:null
-  }
+     {
+          data?.error && <span style={{ color: "red" }}>{data?.error}</span>
+        }
+        {
+          data?.message && <span style={{ color: "green" }}>{data?.message}</span>
+         }
+      <form action={action}>
+        <input defaultValue={data?.name} type="text" placeholder="enter name" name="name" />
+        <br />
+        <br />
+        <input defaultValue={data?.password} type="text" placeholder="enter password" name="password" />
+        <br />
+        <br />
+      
+        <button disabled={pending}>Login</button>
+
+      </form>
 
     </>
   )
